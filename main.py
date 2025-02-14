3ba6110d0253f02135fdba8c006b5220e15a884e
mwaseemz <mwaseemzaheer@gmail.com>
2025-02-14 14:30:43 +0500

    first commit

--- main.py
+++ main.py
@@ -147,5 +147,5 @@ def transcribe():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    # For local development.
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
